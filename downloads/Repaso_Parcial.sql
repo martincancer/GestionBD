678cb16c40eb8b3cd8c840ef2f2c14c50701dce6
martincancer <79979180+martincancer@users.noreply.github.com>
2026-05-13 18:36:09 -0300

    Agregar archivo: Repaso_Parcial.sql

---
+++ downloads/Repaso_Parcial.sql
@@ -0,0 +1,32 @@
+
+--1- se desea saber cuales productos tienen iva general y cuales iva reducido. Para esto crear una consulta que realice esta clasificacion, mostrando el nombre del producto,
+	-- la subcategoria y categoria correspondiente y una columna que indique "IVA GENERAL" para los productos con procentaje de iva al 21 e "IVA REDUCIDO" para los productos
+	-- con porcentaje al 10.5
+
+--2-- obtener las cantidades totales de los productos vendidos y clasificarlas en sobrevendidos para mayores a 30 y subvendido para menores o iguales a 30,
+	-- mostrar producto, cantidad total y clasificacion.
+
+
+--3- clasificar por cantidad de ventas por mes y año:
+	--Bajas cuando sean menor o igual a 50 
+	--Moderadas cuando sean menores o igual a 80
+	--Altas para los demas casos
+	-- Mostrar año, mes, cantidad de ventas y clasificacion
+	-- ordenar por año y mes
+
+
+--4- se necesita obtener la razon social de los clientes que hayan comprado en el mes de junio de 2022. Obtener mediante una subconsulta.
+
+--5- se desea saber que clientes compraron algun IPHONE y en que fecha. Realizarlo mediante una subconsulta.
+
+--6- obtener el stock real de los productos, es decir las cantidades compradas menos las cantidades vendidas.
+	-- mostrar id de producto, nombre y stock real. 
+
+--7- Crear un trigger para la insercion en la tabla Empleados, para que cada vez que se inserte un empleado, le asigne automaticamente el sueldo del puesto 
+-- que se le asigno
+	
+
+
+
+
+
